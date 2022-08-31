c3847956097089f5a31613b4c8b7b462ed9c92fe
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-31 17:59:04 -0400

    Added a component for the footer.

---
+++ components/custom-footer.tsx
@@ -0,0 +1,7 @@
+import styles from './custom-footer.module.css'
+
+export default function CustomFooter() {
+    return <footer className={styles.footer}>
+        <a href="https://github.com/anthonyhuang07/Latin-Variants-Translator">Copyright (c) 2022 Anthony Huang</a>
+    </footer>
+}
