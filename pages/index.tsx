8ed108b98c50bb84f0e3924eb8a00a47a0060647
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-31 17:07:50 -0400

    Added the title.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
+import Head from "next/head";
+import Main from '../components/main'
+import CoolTitle from "../components/cool-title";
+
 export default function Index() {
     return (
         <>
-            <h1>h</h1>
-            <p>h</p>
+            <Head>
+                <title>Hello!</title>
+            </Head>
+
+            <Main>
+                <CoolTitle>
+                    Latin Variants
+                </CoolTitle>
+            </Main>
         </>
     )
 }
