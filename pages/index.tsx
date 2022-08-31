959037dcca0b37a22c06cdda5c7ea2d906194e57
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-31 17:45:09 -0400

    Added the article for the main page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import Head from "next/head";
+import Head from "next/head"
+
 import Main from '../components/main'
-import CoolTitle from "../components/cool-title";
+import CoolTitle from "../components/cool-title"
+import CustomArticle from "../components/custom-article"
 
 export default function Index() {
     return (
@@ -13,6 +15,20 @@ export default function Index() {
                 <CoolTitle>
                     Latin Variants
                 </CoolTitle>
+
+                <CustomArticle>
+                    <h2>What are Latin Variants?</h2><br />
+                    <p>Latin variants are joke languages that people can use for secret messages. Pig Latin, for example. You can find out more about the languages themselves by going to their designated page:</p><br /><br />
+                    <p>
+
+                    </p>
+
+                    <ul>
+                        <li><a href="./pages/egglatin">Egg Latin</a></li>
+                        <li><a href="./pages/piglatin">Pig Latin</a></li>
+                        <li><a href="./pages/bolatin">Bacon & Omelette Latin <abbr title="Translating issues (both forwards and back) do not recommend using for the time being">[BROKEN]</abbr></a></li>
+                    </ul>
+                </CustomArticle>
             </Main>
         </>
     )
