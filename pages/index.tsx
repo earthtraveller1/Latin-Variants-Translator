eae8ff57ff7970434201e0f983cb7ac2b7c3f6cd
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-31 17:53:38 -0400

    Did some formatting and converted some a's to Links.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head"
 import Main from '../components/main'
 import CoolTitle from "../components/cool-title"
 import CustomArticle from "../components/custom-article"
+import Link from "next/link"
 
 export default function Index() {
     return (
@@ -12,20 +13,15 @@ export default function Index() {
             </Head>
 
             <Main>
-                <CoolTitle>
-                    Latin Variants
-                </CoolTitle>
+                <CoolTitle>Latin Variants</CoolTitle>
 
                 <CustomArticle>
                     <h2>What are Latin Variants?</h2><br />
                     <p>Latin variants are joke languages that people can use for secret messages. Pig Latin, for example. You can find out more about the languages themselves by going to their designated page:</p><br /><br />
-                    <p>
-
-                    </p>
-
+                    <p />
                     <ul>
-                        <li><a href="./pages/egglatin">Egg Latin</a></li>
-                        <li><a href="./pages/piglatin">Pig Latin</a></li>
+                        <li><Link href="./pages/egglatin">Egg Latin</Link></li>
+                        <li><Link href="./pages/piglatin">Pig Latin</Link></li>
                         <li><a href="./pages/bolatin">Bacon & Omelette Latin <abbr title="Translating issues (both forwards and back) do not recommend using for the time being">[BROKEN]</abbr></a></li>
                     </ul>
                 </CustomArticle>
